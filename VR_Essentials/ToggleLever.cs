2284571ff39fd6a11c101655547a5d447e65006f
Johannes Hofer <JohannesHofer@gmx.net>
2020-09-26 11:37:37 +0200

    added toggle button script

---
+++ VR_Essentials/ToggleLever.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Events;
+
+/*
+ * Script for toggle-buttons and levers
+ * Not physics interaction compatible yet
+ */
+
+public class ToggleLever : MonoBehaviour
+{
+    
+    public UnityEvent turnedOn, turnedOff;
+    public bool on;
+
+    public void Activate()
+    {
+        on = !on;
+
+        if (on)
+        {
+            turnedOn.Invoke();
+        }else
+        {
+            turnedOff.Invoke();
+        }
+    }
+    
+}
