e6533871cee25e502776cb397b2f045da7d8f03c
Pirasp <johanneshofer@gmx.net>
2020-10-11 14:38:46 +0200

    added Singleton example

---
+++ Utility/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//singleton implementation for game managers
+
+public class Singleton : MonoBehaviour
+{
+    public static Singleton instance;
+    
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }else if (instance != this)
+            {
+                Debug.Log("Instance already exists, destroying object!");
+                Destroy(this);
+            }
+        }
+
+}
